0a79c6e6c9732025d19b5ad3cc8564033fbe1c73
JunheeLee7 <120552023+JunheeLee7@users.noreply.github.com>
2023-02-16 16:01:27 +0900

    Create 1028 정수 1개 입출력(0~).cpp

---
+++ 1028 정수 1개 입출력(0~).cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	unsigned int a; // unsigned int는 int와 같이 a가 정수라는 것을 뜻하고는 있지만, unsigned갑 붙으면서 양의 정수를 뜻함.
+	cin >> a;
+	cout << a << endl;
+}
